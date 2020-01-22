1500fc3f38783c380734aa33429f3b797d80ec2e
João <pintojr@icomp.ufam.edu.br>
2020-01-22 17:12:38 -0400

    added controller of new page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 
+import 'package:contacts/pages/contactPage.dart';
 import 'package:flutter/material.dart';
 import 'pages/homePage.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage(),));
+  runApp(MaterialApp(home: ContactPage()),);
 }
--- lib/pages/contactPage.dart
+++ lib/pages/contactPage.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:contacts/helpers/contactHelper.dart';
 import 'package:flutter/material.dart';
 
@@ -13,12 +15,18 @@ class ContactPage extends StatefulWidget {
 
 class _ContactPageState extends State<ContactPage> {
 
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  bool _userEdited = false;
   Contact _editedContact;
 
   Widget main() {
     return Scaffold(
       appBar: _appBar(),
       floatingActionButton: _floatButton(),
+      body: _body(),
     );
   }
 
@@ -26,6 +34,7 @@ class _ContactPageState extends State<ContactPage> {
     return AppBar(
       backgroundColor: Colors.red,
       title: Text(_editedContact.name ?? "New contact"),
+      centerTitle: true,
     );
   }
 
@@ -35,6 +44,56 @@ class _ContactPageState extends State<ContactPage> {
       child: Icon(Icons.save),
       onPressed: (){},
 
+    );
+  }
+
+  Widget _body() {
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            child:  Container(
+                      width: 140,
+                      height: 140,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: _editedContact.img != null ? FileImage(File(_editedContact.img)): AssetImage("images/person.png")
+                        ),
+                      ),
+                    ),
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: "Name"),
+            controller: _nameController,
+            onChanged: (text){
+              _userEdited = true;
+              setState(() {
+                _editedContact.name = text;
+              });
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: "Email"),
+            keyboardType: TextInputType.emailAddress,
+            controller: _emailController,
+            onChanged: (text){
+              _userEdited = true;
+              _editedContact.email = text;              
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: "Phone"),
+            keyboardType: TextInputType.phone,
+            controller: _phoneController,
+            onChanged: (text){
+              _userEdited = true;
+              _editedContact.phone = text;              
+            },
+          ),
+        ],
+      ),
     );
   }
 
@@ -52,6 +111,10 @@ class _ContactPageState extends State<ContactPage> {
     }
     else {
       _editedContact = Contact.fromMap(widget.contact.toMap());
+      
+      _nameController.text = _editedContact.name;
+      _emailController.text = _editedContact.email;
+      _phoneController.text = _editedContact.phone;
     }
   }
 }
